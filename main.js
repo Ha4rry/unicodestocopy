c901a95c9dd25b731bd0c0c512423c21012933ad
ha4rry <harrymawn@gmail.com>
2023-11-05 14:33:37 +0000

    v1.05
    added combo for invis name for some games

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+let newlinehexvals = (0x0d, 0x0a);
+let combonewlineamt=9;
+
+
 function doIt(uni) {
-    
     if (uni == "smalldot") {
         navigator.clipboard.writeText(String.fromCodePoint(0xFE52));
     }
     else if (uni == "newline") {
-        navigator.clipboard.writeText(String.fromCodePoint(0x000D));
+        navigator.clipboard.writeText(String.fromCodePoint(newlinehexvals));
+    }
+    else if (uni == "combo") {
+        navigator.clipboard.writeText(String.fromCodePoint(0xFE52, newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals,newlinehexvals)); // its not pretty
     }
     
 }
